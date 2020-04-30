09436ee030e616332b84d2848fdee86f485e4b4b
cidospark <cidospark27@gmail.com>
2020-04-30 16:33:29 +0100

    Added action to create new roles

--- CareersListing/Controllers/AdministrationController.cs
+++ CareersListing/Controllers/AdministrationController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CareersListing.Models;
+using CareersListing.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,16 +13,55 @@ namespace CareersListing.Controllers
 {
     public class AdministrationController : Controller
     {
-        // GET: /<controller>/
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
+        {
+            return RedirectToAction("Dashboard", "Administration");
+        }
+
+        // Create role
+        [HttpGet]
+        public IActionResult CreateRole()
         {
             return View();
         }
+        [HttpPost]
+        public async  Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        { 
+            // if model is valid
+            if (ModelState.IsValid)
+            {
+                // create role object using value from model passed in
+                var role = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                // create the role in the database  
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Roles", "Administration");
+                }
+
+            }
+
+            // if model not valid
+            return View(model);
+        }
+        // -------------------------------------------------------- 
 
         [HttpGet]
         public IActionResult Roles()
         {
-            return View();
+            var roles = _roleManager.Roles;
+            return View(roles);
         }
     }
 }
