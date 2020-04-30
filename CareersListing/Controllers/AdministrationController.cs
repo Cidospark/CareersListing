c7dbf1325783b2a1955da51b1271b4663ba4793c
cidospark <cidospark27@gmail.com>
2020-04-30 14:27:26 +0100

    Added list roles view

---
+++ CareersListing/Controllers/AdministrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CareersListing.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Roles()
+        {
+            return View();
+        }
+    }
+}
