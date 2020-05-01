47805bc7bff7c94cb5855c2d4aa4c27045c9157b
cidospark <cidospark27@gmail.com>
2020-05-01 19:30:27 +0100

    Added claims management

---
+++ CareersListing/ViewModels/ClaimsStoreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareersListing.ViewModels
+{
+    public class ClaimsStoreViewModel
+    {
+        public string ClaimType { get; set; }
+
+    }
+}
